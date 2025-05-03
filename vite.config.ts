b5f4d22350e0c741dafaac6d1d87fbd8f223e199
MasakiHM <akan.marimokkori@gmail.com>
2025-05-03 17:53:14 +0900

    Fix Vercel build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { dirname, resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // ★ 追加
